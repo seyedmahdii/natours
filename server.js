5b5123fe961120c1113c09a00dbee29518a27626
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2024-03-22 05:37:38 +0330

    Connect to mongoose

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-dotenv.config({ path: `./config.env` });
+dotenv.config({ path: `./.env` });
 
 const app = require("./app");
 
 // EVERYTHING THAT IS NOT RELATED TO EXPRESS
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
