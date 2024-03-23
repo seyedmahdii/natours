5e8c8a96b0e0e83a41be49871ee16d24bcf488c9
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2024-03-23 07:35:33 +0330

    Connect delete tour route-handler to mongodn

--- controllers/tours.js
+++ controllers/tours.js
@@ -103,13 +103,19 @@ const updateTour = async (req, res) => {
   }
 };
 
-const deleteTour = (req, res) => {
-  // const id = Number(req.params.id);
-
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
+const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
 
 module.exports = {
