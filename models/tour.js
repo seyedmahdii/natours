ee3d652d0b3d44fe14187f919bfc442bc36b3abc
seyedmahdii <seyedmahdijalali2020@gmail.com>
2024-03-29 06:48:44 +0330

    Add mongoose document middleware to add slug property

--- models/tour.js
+++ models/tour.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const tourSchema = mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A tour must have a duration"],
@@ -66,6 +68,21 @@ const tourSchema = mongoose.Schema(
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
+
+// pre middleware: it's gonna run before an actual event
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre("save", function (next) {
+  // this here is the created document
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// runs after all the pre middleware functions have completed
+// tourSchema.post("save", function (doc, next) {
+//   // this here is the created document
+//   this.slug = slugify(this.name, { lower: true });
+//   next();
+// });
 
 const Tour = mongoose.model("Tour", tourSchema);
 
