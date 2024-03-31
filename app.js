c633aee77035081c7863feb3e2b8447f59f2905c
seyedmahdii <seyedmahdijalali2020@gmail.com>
2024-03-31 05:08:59 +0330

    Add * route

--- app.js
+++ app.js
@@ -16,4 +16,12 @@ app.use(express.static(`${__dirname}/public`));
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+// If we get to the route *, it means that none of the routes middleware we matched
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this Server!`,
+  });
+});
+
 module.exports = app;
